461590d9f0738a809a1cda582eb5d0cde697bb48
rakshith.uk <rakshithuk@gmail.com>
2021-11-23 16:29:39 +0530

    redirect

--- force-app/main/default/lwc/hyperLinkProducts/hyperLinkProducts.js
+++ force-app/main/default/lwc/hyperLinkProducts/hyperLinkProducts.js
@@ -1,10 +1,25 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import Id from '@salesforce/user/Id';
+import basePath from '@salesforce/community/basePath';
+import { NavigationMixin } from 'lightning/navigation';
 
 export default class HyperLinkProducts extends LightningElement {
     @api applyNow;
     @track isModalOpen = false;
+    @wire(basePath) pageRef;
+    userId = Id;
 
     handleClick(){
+        if(!this.userId){
+            this[NavigationMixin.GenerateUrl]({
+                type: 'standard__webPage',
+                attributes: {
+                    url: this.pageref+'/login/'
+                }
+            }).then(generatedUrl => {
+                window.open(generatedUrl);
+            });
+        }
         this.isModalOpen = true;
     }
     closeModal() {
