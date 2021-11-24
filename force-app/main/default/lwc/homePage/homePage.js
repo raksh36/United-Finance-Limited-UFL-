c14d63d58e41d61c82afc78152c8ba52279ffcc4
rakshith.uk <rakshithuk@gmail.com>
2021-11-24 15:24:12 +0530

    carousel

--- force-app/main/default/lwc/homePage/homePage.js
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -1,5 +1,11 @@
 import { LightningElement, api  } from 'lwc';
+import EMI from '@salesforce/resourceUrl/EMI';
+import Truck from '@salesforce/resourceUrl/Truck';
+import MobileApp from '@salesforce/resourceUrl/MobileApp';
 
 export default class HomePage extends LightningElement {
     @api buttonText;
+    emi = EMI;
+    truck = Truck;
+    mobileApp = MobileApp;
 }
